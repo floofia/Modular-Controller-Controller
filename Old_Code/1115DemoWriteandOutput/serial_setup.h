6ed8fc724f6b4509b8a08cac72a2bf6f2dcb7a9d
Duncan-McG <duncan.mcgonagle@mines.sdsmt.edu>
2023-02-17 13:06:50 -0700

    Removed Old Code Folder

--- Old_Code/1115DemoWriteandOutput/serial_setup.h
+++
@@ -1,9 +0,0 @@
-//Serial Setup
-
-void serial_setup(int baud)
-{
-Serial.begin(baud);
-while (!Serial);             // Leonardo: wait for serial monitor
-Serial.setTimeout(3000);
-Serial.println("\nI2C Scanner and more!");
-}
