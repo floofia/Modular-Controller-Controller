1c79c9ef3c8bf19e8392feeed0c2143ca9dc94da
Sofia Sadun <Sofia.Sadun@mines.sdsmt.edu>
2023-02-22 15:56:38 -0700

    pushing in case there's something that needs it

--- Controller_Code/Sofias Headers/modulefunc.h
+++ Controller_Code/Sofias Headers/modulefunc.h
@@ -6,6 +6,6 @@
 void faceButtonSetup (int top, int bottom, int left, int right, Adafruit_seesaw ss);
 void faceButtonRead (int top, int bottom, int left, int right, Adafruit_seesaw ss);
 void dpadSetup (int up, int down, int left, int right, Adafruit_seesaw ss);
-void dpadRead (int down, int down, int left, int right, Adafruit_seesaw ss);
+void dpadRead (int up, int down, int left, int right, Adafruit_seesaw ss);
 void joystickSetup(int x, int y, int select, Adafruit_seesaw ss);
 void joystickRead(int x, int y, int select, Adafruit_seesaw ss);
