dec8adae8ac541152b011362395b0932aadc20b7
Sofia Sadun <Sofia.Sadun@mines.sdsmt.edu>
2023-02-24 13:06:44 -0700

    added to files

--- Controller_Code/Sofias Headers/modulefunc.c
+++ Controller_Code/Sofias Headers/modulefunc.c
@@ -2,7 +2,10 @@
 
 
 
-void faceButtonSetup (int top, int bottom, int left, int right, Adafruit_seesaw ss)
+void faceButtonSetup (int top, int bottom, int left, int right, char address, Adafruit_seesaw ss)
 {
-
+  Wire.begin();
+  ss.begin(address); // The default address for the seesaw breakout board
+  // Initialize the BLEGamepad
+  gamepad.begin();
 }
