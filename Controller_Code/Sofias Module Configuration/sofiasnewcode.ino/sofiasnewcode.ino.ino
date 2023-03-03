a610cd3e471ad8b8520787f7fcb2122f6708c583
Sofia Sadun <Sofia.Sadun@mines.sdsmt.edu>
2023-03-03 10:04:01 -0700

    Put in all of the new functional code

---
+++ Controller_Code/Sofias Module Configuration/sofiasnewcode.ino/sofiasnewcode.ino.ino
@@ -0,0 +1,12 @@
+
+#include "modulefunctiondefinitions.h"
+#include "modulefuncheader.h"
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
